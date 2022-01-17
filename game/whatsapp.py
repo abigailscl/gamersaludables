22adf1400b8ff36dbc874362f9629cd38d669606
sacabascango1 <sacabascango1@espe.edu.ec>
2022-01-17 05:39:55 -0500

    implement whatsapp api

--- game/whatsapp.py
+++ game/whatsapp.py
@@ -1,8 +1,11 @@
 import pywhatkit
+class Whatsapp():
+    def __init__(self):
+        self.message = ""
+        self.number = ""
+        self.hour = 0
+        self.minutes = 0
+    def send_mesage(self):
+        pywhatkit.sendwhatmsg(self.number,self.message, self.hour, self.minutes)
 
-try:
-    pywhatkit.sendwhatmsg("+593 96 308 5404", "Hola mi amor te escribo desde mi video juego xd",22,35)
-    print("Abriendo Escanee Codigo QR")
-    print("enviado exitosamente ")
-except:
-    print("no se pudo encontrar")
+
